fdcf5911166bf6d088526f3b84fe5cd2c17e1717
faetalize <faetalize@pm.me>
2025-08-15 08:39:11 +0100

    fix: Add missing rollupOptions input configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,12 @@ export default {
         target: 'esnext',
         outDir: '../dist',
         emptyOutDir: true,
+        rollupOptions:{
+            input: {
+                main: './src/index.html',
+                privacy: './src/privacy-policy.html',
+                terms: './src/terms-of-service.html'
+            }
+        }
     }
 }
