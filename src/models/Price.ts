8ed01e129ac73ede250b6a627f36ab6b8c5ac6c6
faetalize <faetalize@pm.me>
2025-11-01 04:23:08 +0100

    refactor: Rename SubscriptionPriceIDs to SubscriptionPriceIDsOld and introduce new SubscriptionPriceIDs enum

--- src/models/Price.ts
+++ src/models/Price.ts
@@ -1,6 +1,13 @@
-export enum SubscriptionPriceIDs {
+export enum SubscriptionPriceIDsOld {
     PRO_MONTHLY = 'price_1SDdbKGiJrKwXclR7hn7fF4s',
     PRO_YEARLY = 'price_1SDeIFGiJrKwXclRCNThnoXH',
     MAX_MONTHLY = 'price_1SDf2NGiJrKwXclRwDs7XOd0',
     MAX_YEARLY = 'price_1SDf2rGiJrKwXclReGeg8fQo'
+}
+
+export enum SubscriptionPriceIDs {
+    PRO_MONTHLY = 'price_1SOU2lKcI9PDo3JBhsT8URS9',
+    PRO_YEARLY = 'price_1SOU2lKcI9PDo3JBhsT8URS9',
+    MAX_MONTHLY = 'price_1SDf2NGiJrKwXclRwDs7XOd0', //to be updated
+    MAX_YEARLY = 'price_1SDf2rGiJrKwXclReGeg8fQo' //to be updated
 }
