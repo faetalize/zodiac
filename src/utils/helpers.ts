d7720b5ad4fc4346e998ce35b80a28883eaf33cb
faetalize <faetalize@pm.me>
2026-01-28 22:36:15 +0100

    bump version to 1.5.0

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -57,7 +57,7 @@ export function lightenCard(element: HTMLElement) {
 }
 
 export function getVersion() {
-    return "1.2.1";
+    return "1.5.0";
 }
 
 export function getSanitized(string: string) {
