5d13e226c7841470453376ed6a01130bc5ccef77
faetalize <faetalize@pm.me>
2026-03-18 12:37:27 +0000

    chore: bump version to 1.7.1

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -57,7 +57,7 @@ export function lightenCard(element: HTMLElement) {
 }
 
 export function getVersion() {
-    return "1.7.0";
+    return "1.7.1";
 }
 
 export function getSanitized(string: string) {
