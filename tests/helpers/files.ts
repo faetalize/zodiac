215410b254f06e91fcf5c8d1932ce590dd2c23ae
faetalize <faetalize@pm.me>
2026-03-31 18:44:36 +0100

    test: cover attachment drop workflows

--- tests/helpers/files.ts
+++ tests/helpers/files.ts
@@ -1,10 +1,21 @@
 export class MockDataTransfer {
     private readonly filesInternal: File[] = [];
 
+    readonly types = ["Files"];
+
     readonly items = {
         add: (file: File) => {
             this.filesInternal.push(file);
         },
+        [Symbol.iterator]: function* (this: MockDataTransfer) {
+            for (const file of this.filesInternal) {
+                yield {
+                    kind: "file",
+                    type: file.type,
+                    getAsFile: () => file,
+                };
+            }
+        }.bind(this),
     };
 
     get files(): FileList {
@@ -14,4 +25,8 @@ export class MockDataTransfer {
 
 export function makeEmptyFileList(): FileList {
     return [] as unknown as FileList;
+}
+
+export function makeFileList(files: File[]): FileList {
+    return files as unknown as FileList;
 }
