cb37f11cf017239cc913655271f9a3b62a8b6e19
faetalize <faetalize@pm.me>
2026-05-06 15:12:46 +0100

    fix: remove unused Page type import in dynamic group chat test

--- tests/e2e/messages/dynamic-group-chat-byok.spec.ts
+++ tests/e2e/messages/dynamic-group-chat-byok.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test, type Page } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { seedLocalSettings, stubExternalTraffic } from "../helpers/app";
 
 // async function createDynamicGroupChat(page: Page): Promise<string> {
